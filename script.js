0b607e91e33ffb18c1e0825f8dcba367b986405d
Artyom <Korvin1312b@yandex.by>
2021-02-08 13:06:50 +0300

    feat: add new language Russian

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const matrix = createMatrix();
+const matrixEng = createMatrix("english");
+const matrixRus = createMatrix("russian");
+var matrix = matrixEng;
 class VigenereCipheringMachine {
   constructor(bool) {
     if (bool === false) {
@@ -12,12 +14,30 @@ class VigenereCipheringMachine {
       throw new Error();
     }
     let crypt = [];
-    let mes = message.toUpperCase().split("");
+    let mes = [];
     let arrKey = createKey(key, message);
+    let firstLetter;
+    let lastLetter;
+    if (matrix == matrixEng) {
+      mes = message.toUpperCase().split("");
+      firstLetter = 65;
+      lastLetter = 90;
+    } else if (matrix == matrixRus) {
+      mes = message.toLowerCase().split("");
+      firstLetter = 1072;
+      lastLetter = 1105;
+    }
     for (i = 0; i < mes.length; i++) {
-      if (mes[i].charCodeAt(0) >= 65 && mes[i].charCodeAt(0) <= 90) {
+      console.log(mes[i].charCodeAt(0));
+      if (
+        mes[i].charCodeAt(0) >= firstLetter &&
+        mes[i].charCodeAt(0) <= lastLetter
+      ) {
+        console.log(arrKey[i].charCodeAt(0) - firstLetter);
         let letter =
-          matrix[arrKey[i].charCodeAt(0) - 65][mes[i].charCodeAt(0) - 65];
+          matrix[arrKey[i].charCodeAt(0) - firstLetter][
+            mes[i].charCodeAt(0) - firstLetter
+          ];
         crypt.push(letter);
       } else {
         crypt.push(mes[i]);
@@ -32,11 +52,25 @@ class VigenereCipheringMachine {
       throw new Error();
     }
     let crypt = [];
-    let mes = encryptedMessage.toUpperCase().split("");
+    let mes = [];
     let arrKey = createKey(key, encryptedMessage);
+    let firstLetter;
+    let lastLetter;
+    if (matrix == matrixEng) {
+      mes = encryptedMessage.toUpperCase().split("");
+      firstLetter = 65;
+      lastLetter = 90;
+    } else if (matrix == matrixRus) {
+      mes = encryptedMessage.toLowerCase().split("");
+      firstLetter = 1072;
+      lastLetter = 1105;
+    }
     for (i = 0; i < mes.length; i++) {
-      if (mes[i].charCodeAt(0) >= 65 && mes[i].charCodeAt(0) <= 90) {
-        let pos = matrix[arrKey[i].charCodeAt(0) - 65].indexOf(mes[i]);
+      if (
+        mes[i].charCodeAt(0) >= firstLetter &&
+        mes[i].charCodeAt(0) <= lastLetter
+      ) {
+        let pos = matrix[arrKey[i].charCodeAt(0) - firstLetter].indexOf(mes[i]);
         let letter = matrix[0][pos];
         crypt.push(letter);
       } else {
@@ -49,18 +83,32 @@ class VigenereCipheringMachine {
   }
 }
 
-function createMatrix() {
+function createMatrix(lang) {
   let matrix = [];
-  for (j = 0; j < 26; j++) {
-    matrix[j] = [];
-    for (i = 0; i < 26; i++) {
-      let arrPos = i + 65 + j;
-      if (arrPos > 90) {
-        arrPos = i + 65 + j - 26;
+  if (lang == "english") {
+    for (j = 0; j < 26; j++) {
+      matrix[j] = [];
+      for (i = 0; i < 26; i++) {
+        let arrPos = i + 65 + j;
+        if (arrPos > 90) {
+          arrPos = i + 65 + j - 26;
+        }
+        matrix[j].push(String.fromCharCode(arrPos));
       }
-      matrix[j].push(String.fromCharCode(arrPos));
+    }
+  } else if (lang == "russian") {
+    for (j = 0; j < 34; j++) {
+      matrix[j] = [];
+      for (i = 0; i < 34; i++) {
+        let arrPos = i + 1072 + j;
+        if (arrPos > 1105) {
+          arrPos = i + 1072 + j - 34;
+        }
+        matrix[j].push(String.fromCharCode(arrPos));
+      }
     }
   }
+
   return matrix;
 }
 
@@ -71,8 +119,11 @@ function createKey(key, message) {
   while (key.length > message.length) {
     key = key.slice(0, key.length - 1);
   }
-
-  return key.toUpperCase().split("");
+  if (matrix == matrixEng) {
+    return key.toUpperCase().split("");
+  } else if (matrix == matrixRus) {
+    return key.toLowerCase().split("");
+  }
 }
 
 function checkTypeAnswer(type, crypt) {
@@ -97,3 +148,23 @@ document.getElementById("decryptBtn").onclick = function () {
     secretWord.value
   );
 };
+
+langSection.addEventListener("click", function (event) {
+  if (event.target.classList.value == "langFiltr") {
+    let langFiltrs = document.getElementsByClassName("langFiltr");
+    for (let btn2 of langFiltrs) {
+      btn2.style.backgroundColor = "white";
+    }
+    event.target.style.backgroundColor = "rgb(89, 187, 187)";
+    switch (event.target.id) {
+      case "langEnglish":
+        matrix = matrixEng;
+        break;
+      case "langRussian":
+        matrix = matrixRus;
+        break;
+      default:
+        break;
+    }
+  }
+});
